b26e8a4e023469d4aa3bbb71cd0180fc90bb49f5
Délita Makanda <delita.makanda@gmail.com>
2025-06-28 18:49:02 +0200

    serialization

--- src/LoyaltyProgram.Domain/Client.cs
+++ src/LoyaltyProgram.Domain/Client.cs
@@ -1,13 +1,24 @@
+using System.Text.Json.Serialization;
 using LoyaltyProgram.Domain;
 
 public class Client
 {
+    [JsonPropertyName("client_id")]
     public int ClientId { get; set; }
+    [JsonPropertyName("first_name")]
     public string? FirstName { get; set; }
+    [JsonPropertyName("last_name")]
     public string? LastName { get; set; }
+    [JsonPropertyName("address")]
+    public string? Address { get; set; }
+    [JsonPropertyName("email")]
     public string? Email { get; set; }
+    [JsonPropertyName("phone_number")]
     public string? PhoneNumber { get; set; }
+    [JsonPropertyName("date_of_birth")]
     public DateTime DateOfBirth { get; set; }
+    [JsonPropertyName("date_created")]
     public DateTime? DateCreated { get; set; }
+    [JsonPropertyName("loyalty_card")]
     public LoyaltyCard? LoyaltyCard { get; set; }
 }
